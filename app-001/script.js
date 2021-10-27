7c09ba92b28e3870095a2b5e8e1cc992e4b7f8d5
vitalii boiko <vukelan@gmail.com>
2021-10-27 23:09:23 +0300

    output test cases

--- app-001/script.js
+++ app-001/script.js
@@ -1,12 +1,91 @@
+
 (function() {
-    const rootElement = document.getElementById('root');
-    
+    const testArray1 = [
+        {
+          "from":"03/01/2021",
+          "to":"03/06/2021"
+        },
+        {
+          "from":"03/10/2021",
+          "to":"03/15/2021"
+        },
+        {
+          "from":"03/20/2021",
+          "to":"03/25/2021"
+        }
+    ];
+    const testArray2 = [
+        {
+          "from":"03/01/2021",
+          "to":"03/05/2021"
+        },
+        {
+          "from":"03/08/2021",
+          "to":"03/10/2021"
+        },
+        {
+          "from":"03/07/2021",
+          "to":"03/20/2021"
+        }
+    ];
+    const testArray3 = [
+        {
+          "from":"03/01/2021",
+          "to":"03/05/2021"
+        },
+        {
+          "from":"03/04/2021",
+          "to":"03/10/2021"
+        },
+        {
+          "from":"03/07/2021",
+          "to":"03/20/2021"
+        },
+        {
+          "from":"03/20/2021",
+          "to":"03/30/2021"
+        }
+    ];
+    const testArray4 = [
+        {
+            "from":"03/01/2021",
+            "to":"03/05/2021"
+        },
+        {
+            "from":"03/06/2021",
+            "to":"03/08/2021"
+        },
+        {
+            "from":"03/15/2021",
+            "to":"03/18/2021"
+        },
+        {
+            "from":"03/16/2021",
+            "to":"03/28/2021"
+        }
+    ];
+
+    const testCases = [testArray1, testArray2];
+    var testCaseCounter = 0;
+
+    for (const testCase of testCases) {
+        let headLine = document.createElement('h4');
+        headLine.innerText = 'Test Case #' + testCaseCounter++;
+        document.body.appendChild(headLine);
+
+
+        for (const dateObj of testCase) {
+            let inputData = document.createElement('p');
+            inputData.innerText = JSON.stringify(dateObj);
+            document.body.appendChild(inputData);
+        }
+    }
 })();
 
 function onForm1ButtonClick() {
 
     var userInput = document.getElementById('form_1').elements.namedItem('text').value;
-    const userInputOutput = document.getElementById('userInputOutput');
+    const userInputOutput = document.getElementById('output_users_input');
 
     userInputOutput.innerText = 'you entered: ' + userInput;
 
