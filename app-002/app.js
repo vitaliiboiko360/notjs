18827db7416841ac3ea33111c754a81f243503a0
vitaliiboiko360 <vukelan@gmail.com>
2022-07-12 21:40:33 +0300

    sync read config

--- app-002/app.js
+++ app-002/app.js
@@ -1,9 +1,21 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
 
-(async () => {
+const config = fs.readFileSync('config.json', 'utf-8');
+console.log(config);
+
+async function startBr(config) {
+    const { stdout, stderr } = await exec(`${config.osxExecPath} ${config.cmdArgs}`);
+    console.log('stdout:', stdout);
+    console.error('stderr:', stderr);
+}
+
+async function doApp() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto(config.url);
 
   // Get the "viewport" of the page, as reported by the page.
   const dimensions = await page.evaluate(() => {
@@ -11,10 +23,12 @@ const puppeteer = require('puppeteer');
       width: document.documentElement.clientWidth,
       height: document.documentElement.clientHeight,
       deviceScaleFactor: window.devicePixelRatio,
+      headless: false,
+      executablePath: config.osxExecPath,
     };
   });
 
   console.log('Dimensions:', dimensions);
 
   await browser.close();
-})();
+};
