c3a26cc47c55e8d087defe9a3762c720a287d771
vitaliiboiko360 <vukelan@gmail.com>
2022-07-14 22:31:07 +0300

    change to stdout

--- app-002/app.js
+++ app-002/app.js
@@ -4,34 +4,32 @@ const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 // const spawn = util.promisify(require('node:child_process').spawn);
 const spawn = require('node:child_process').spawn;
+const spawnSync = require('node:child_process').spawnSync;
 var stream = require('stream');
 const { start } = require('repl');
 
 const json = fs.readFileSync('config.json', 'utf-8');
 const config = JSON.parse(json);
-console.log(config);
-
-
 
 function startBr(config) {
     let cmd = `"${config.execPath}" ${config.cmdArgs.join(' ')}`;
     console.log(`running ${cmd}`);
     const child = spawn(config.execPath, config.cmdArgs, {
         detached: true,
-        stdio: 'pipe'
+        stdio: ['ignore', 'pipe', 'ignore']
     });
-    child.unref();
-
+    
     var buffer = Buffer.alloc(0);
-    child.stdin.on('data', function(data) {
+    child.stdout.on('data', (data)=>{
         console.log(`on data: ${data}`);
-        buffer = Buffer.concat([buffer, data]);
-        console.log(`on data: ${buffer.toString()}`);
+        // buffer = Buffer.concat([buffer, data]);
+        // console.log(`on data: ${buffer.toString()}`);
     });
 
-    console.log('after on data handler');
-    console.log('before return');
     console.log(`buffer: ${buffer.toString()}`);
+
+    child.unref();
+    
     return buffer.toString();
 }
 
