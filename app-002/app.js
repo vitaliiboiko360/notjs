a7c2d2deea7806f35c0ff18bb0c73e48d9be42fb
vitaliiboiko360 <vukelan@gmail.com>
2022-07-15 22:08:15 +0300

    working example

--- app-002/app.js
+++ app-002/app.js
@@ -29,19 +29,18 @@ function startBr(config) {
     var output = Buffer.alloc(0);
     child.stderr.on('data', (data)=>{
         let match = re.exec(data.toString());
-        if(match) { url.ready = true; url.url = match[0]; };
+        if(match) { 
+            url.ready = true; 
+            url.url = match[0]; 
+        };
     });
 }
 
-async function getUrl() {
-    let output = await startBr();
-    console.log(output);
-}
-
-async function doApp(config) {
-  const browser = await puppeteer.launch();
+async function doApp(config, ws) {
+  console.log(`doApp started with ${ws}`);
+  const browser = await puppeteer.connect({browserWSEndpoint:ws});
   const page = await browser.newPage();
-  await page.goto(config.url);
+  await page.goto(config.url, {waitUntil: 'networkidle2'});
 
   // Get the "viewport" of the page, as reported by the page.
   const dimensions = await page.evaluate(() => {
@@ -49,14 +48,10 @@ async function doApp(config) {
       width: document.documentElement.clientWidth,
       height: document.documentElement.clientHeight,
       deviceScaleFactor: window.devicePixelRatio,
-      headless: false,
-      executablePath: config.execPath,
     };
   });
 
   console.log('Dimensions:', dimensions);
-
-  await browser.close();
 };
 
 (function(){
@@ -66,9 +61,10 @@ async function doApp(config) {
     let id = setInterval(()=>{
         if (url.ready) {
             console.log(`we have url ${url.url}`);
-            console.log('we are ready');
+            console.log('start')
             clearInterval(id);
-        } else console.log('...waiting for url');
+            doApp(config, url.url);
+        }
     },1000);
-    
+
 })();
