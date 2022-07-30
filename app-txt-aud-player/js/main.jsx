81cdb7bd5c128f5fc6005ee6b4a3e09f07dcd17a
vitaliiboiko360 <vukelan@gmail.com>
2022-07-31 00:54:54 +0300

    upgrade to 18 react renderRoot

--- app-txt-aud-player/js/main.jsx
+++ app-txt-aud-player/js/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactAudioPlayer from '../src/index.tsx';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <ReactAudioPlayer
-    src="/files/George_Gershwin_playing_Rhapsody_in_Blue.ogg"
-    controls
-  />,
-  document.querySelector('.app'),
-);
+const container = document.getElementById('app');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<ReactAudioPlayer
+  src="threepigs-sp-full-128.mp3"
+  controls
+/>);
