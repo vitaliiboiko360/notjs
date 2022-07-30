dab6a0532412c20076eb0220892972ab8cd17446
vitaliiboiko360 <vukelan@gmail.com>
2022-07-30 22:58:51 +0300

    resolve to module name

--- app-txt-aud-player/js/main.jsx
+++ app-txt-aud-player/js/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactAudioPlayer from 'react-audio-player';
+import ReactAudioPlayer from '../src/index.tsx';
 
 ReactDOM.render(
   <ReactAudioPlayer
