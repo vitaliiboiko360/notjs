4e444954850938c2d3fbbd71b75fe0f0d87a6b46
vitaliiboiko360 <vukelan@gmail.com>
2022-08-06 19:13:43 +0300

    add slider

---
+++ app-txt-aud-player/src/slider.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import Slider, { SliderThumb, SliderValueLabelProps } from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const marks = [
+    {
+      value: 0,
+    },
+    {
+      value: 100,
+    },
+  ];
+
+  const PositionSlider = styled(Slider)(({ theme }) => ({
+    color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
+    height: 2,
+    padding: '15px 0',
+    '& .MuiSlider-thumb': {
+      height: 28,
+      width: 28,
+      backgroundColor: '#fff',
+      boxShadow: iOSBoxShadow,
+      '&:focus, &:hover, &.Mui-active': {
+        boxShadow:
+          '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          boxShadow: iOSBoxShadow,
+        },
+      },
+    },
+    '& .MuiSlider-valueLabel': {
+      fontSize: 12,
+      fontWeight: 'normal',
+      top: -6,
+      backgroundColor: 'unset',
+      color: theme.palette.text.primary,
+      '&:before': {
+        display: 'none',
+      },
+      '& *': {
+        background: 'transparent',
+        color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+      },
+    },
+    '& .MuiSlider-track': {
+      border: 'none',
+    },
+    '& .MuiSlider-rail': {
+      opacity: 0.5,
+      backgroundColor: '#bfbfbf',
+    },
+    '& .MuiSlider-mark': {
+      backgroundColor: '#bfbfbf',
+      height: 8,
+      width: 1,
+      '&.MuiSlider-markActive': {
+        opacity: 1,
+        backgroundColor: 'currentColor',
+      },
+    },
+  }));
+
+  export default function PositionSlider() {
+    return (
+      <Box sx={{ width: 320 }}>
+        <Typography gutterBottom>iOS</Typography>
+        <PositionSlider
+          aria-label="ios slider"
+          defaultValue={60}
+          marks={marks}
+          valueLabelDisplay="on"
+        />
+      </Box>
+    );
+  }
