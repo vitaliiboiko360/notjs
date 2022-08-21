6eedee46f05f38c74db260b8e15e7e0eee0431af
vitaliiboiko360 <vukelan@gmail.com>
2022-08-21 22:17:50 +0300

    add express file srv

--- app-txt-aud-player/src/data_api.ts
+++ app-txt-aud-player/src/data_api.ts
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'data')));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port);
+console.log(`Listening on port ${port}`);
