edd7f2788c057c36f2870a4e59b5c47e62548470
vitaliiboiko360 <vukelan@gmail.com>
2022-08-22 22:01:34 +0300

    fix static file srv

--- app-txt-aud-player/src/data_api.ts
+++ app-txt-aud-player/src/data_api.ts
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const fs = require('fs');
+
+const dataPath = path.join(__dirname, '../data');
 
 //app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'data')));
+app.use('/data', express.static(dataPath));
 
 app.listen(port);
-console.log(`Listening on port ${port}`);
+
+console.log(`data path is: ${dataPath}`);
+
+const files = fs.readdirSync(dataPath, {withFileTypes:false});
+
+files.forEach(element => {
+    console.log(`http://localhost:${port}/data/${element}`);
+});
+
