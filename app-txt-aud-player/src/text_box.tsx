d6485acf1a35f36b74dfdf8e1f2e595c77164232
vitaliiboiko360 <vukelan@gmail.com>
2022-09-09 22:07:18 +0300

    add border

--- app-txt-aud-player/src/text_box.tsx
+++ app-txt-aud-player/src/text_box.tsx
@@ -4,7 +4,9 @@ import { styled } from '@mui/material/styles';
 const Div = styled('div')(({ theme }) => ({
   ...theme.typography.h4,
   backgroundColor: theme.palette.background.paper,
-  padding: theme.spacing(1),
+  padding: theme.spacing(3),
+  border: '1px solid blue',
+  borderRadius: '25px',
 }));
 
 export default function TextBox(props) {
