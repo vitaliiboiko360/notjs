9d7b97d6cf9f78242e2e61b72ee1b98ea4939744
vitaliiboiko360 <vukelan@gmail.com>
2023-01-06 18:44:12 +0200

    grid works

--- app-txt-aud-player/src/grid_text.tsx
+++ app-txt-aud-player/src/grid_text.tsx
@@ -8,7 +8,6 @@ import Typography from '@mui/material/Typography';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
   padding: theme.spacing(2),
 /*  maxWidth: 400,*/
   color: theme.palette.text.primary,
@@ -17,7 +16,7 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 export default function AutoGridNoWrap(props) {
   console.log(` recived lines ${props.lines.length}`);
   let linesContent = props.lines.map(line => {
-        <StyledPaper
+        return(<StyledPaper
         sx={{
         my: 1,
         mx: 'auto',
@@ -26,10 +25,13 @@ export default function AutoGridNoWrap(props) {
     >
         <Grid container wrap="nowrap" spacing={2}>
         <Grid item xs zeroMinWidth>
-            <Typography noWrap>{line}</Typography>
+            <Typography sx={{
+              lineHeight: 1.3,
+              fontSize: 23,
+            }}>{line}</Typography>
         </Grid>
         </Grid>
-    </StyledPaper>
+    </StyledPaper>);
   });
   return (
     <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
