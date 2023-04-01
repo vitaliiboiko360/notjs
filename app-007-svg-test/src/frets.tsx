add8cb75f025faf187bacee710312c37d5f65e3e
vitaliiboiko360 <vukelan@gmail.com>
2023-04-01 15:01:40 +0300

    key warning

--- app-007-svg-test/src/frets.tsx
+++ app-007-svg-test/src/frets.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 function createLines() {
   let frets = Array(9).fill(0);
   frets = frets.map((value, index)=>{
-    return `<line x1="${(index+1)*80}" y1="220" x2="${(index+1)*80}" y2="380" stroke="black" />`
+    return `<line key=${index+100} x1="${(index+1)*80}" y1="220" x2="${(index+1)*80}" y2="380" stroke="black" />`
   });
   return {__html: frets.join('')};
 }
--- app-007-svg-test/src/strings.tsx
+++ app-007-svg-test/src/strings.tsx
@@ -3,7 +3,8 @@ import {createElement} from 'react';
 export default function String() {
   const strings = Array.apply(null, {length: 6}).map((element, index)=>{
     const deltaY = index*25;
-    return createElement('line', {x1:0, y1:238+deltaY, x2:800, y2:238+deltaY, stroke: 'black' });
+    const key = index+10;
+    return createElement('line', {key:key, x1:0, y1:238+deltaY, x2:800, y2:238+deltaY, stroke: 'black' });
   });
   return (
     strings
