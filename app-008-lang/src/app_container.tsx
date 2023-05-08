766dbdf3a2325c24a316d04e7f51b69ffe88d891
vitaliiboiko360 <vukelan@gmail.com>
2023-05-08 21:40:03 +0300

    working with react query

--- app-008-lang/src/app_container.tsx
+++ app-008-lang/src/app_container.tsx
@@ -1,58 +1,54 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 const theme = createTheme({
-    typography: {
-      // In Chinese and Japanese the characters are usually larger,
-      // so a smaller fontsize may be appropriate.
-      fontSize: 18,
-    },
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: 18,
+  },
+});
+
+const queryClient = new QueryClient()
+
+function TextLines() {
+  const { isLoading, error, data } = useQuery('text', () =>
+    fetch('http://localhost:4001/data/los_tres_cerditos.txt').then(res =>
+      res.text()
+    )
+  )
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  console.log(data);
+  let lineArray = data.split('\n');
+  let textLines = lineArray.map((textLine, index) => {
+    return (<React.Fragment key={index}>
+      <Typography variant="body1" >
+        {textLine}
+      </Typography>
+    </React.Fragment>);
   });
+  console.log(`textLines.lenght=${textLines.length}`);
+  console.log(`textLines = ${textLines}`);
+  return (<div>{textLines}</div>);
+}
 
 function AppContainer() {
-    const [isTextLoaded, setTextIsLoaded] = useState(false);
-    const [text, setText] = useState('');
-
-    useEffect(() => {
-        const fetchData = async () => {
-            await fetch('http://localhost:4001/data/los_tres_cerditos.txt')
-            .then((response) => {
-                console.log('get response');
-                return response.text();
-            })
-            .then((loadedText)=>{
-            if(!isTextLoaded)
-                setText(loadedText);
-                console.log(`setText called text.lenght=${text.length}`);
-            })
-            .catch((error)=>{
-                console.log(error);
-            });
-        }
-   
-        fetchData();
-        setTextIsLoaded(true);
-      });
-
-    let lineArray = text.split('\n');
-    let textLines = lineArray.map((textLine, index)=>{
-        <React.Fragment key={index}>
-          <Typography variant="body1" >
-              {textLine}
-          </Typography>
-        </React.Fragment>});
-    console.log(`textLines.lenght=${textLines.length}`);
-    console.log(`textLines = ${textLines}`);
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container>
-                {textLines}
-            </Container>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <QueryClientProvider client={queryClient}>
+          <TextLines />
+        </QueryClientProvider>
+      </Container>
+    </ThemeProvider >
+  );
 }
 
 export default AppContainer;
