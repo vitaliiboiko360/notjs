c7739c6b43981a1b4aa2aef3ae2425db7ec41d1b
vitaliiboiko360 <vukelan@gmail.com>
2023-06-19 22:26:28 +0300

    need customizing active text chunk

--- app-008-lang/src/click_lines.tsx
+++ app-008-lang/src/click_lines.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: 12,
+  },
+});
+
 function ClickableLine(props) {
   const onClick = () => { props.onClick(props.index) };
   return (<Typography onClick={onClick} variant="body1" >
