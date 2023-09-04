b9c5d7cebc998a7f1018b5e02ebba55634288f29
vitaliiboiko360 <vukelan@gmail.com>
2023-09-04 21:11:19 +0300

    add slider

--- app-008-lang/src/audio_textlines.tsx
+++ app-008-lang/src/audio_textlines.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import { useRef } from 'react';
+import { useTheme } from '@mui/material/styles';
+
+import Slider from '@mui/material/Slider';
 
 import TextLines from './text_lines.tsx';
 import Audio from './audio.tsx';
 
 export default function AudioTextLines() {
+  const theme = useTheme();
   const numChunks = 26; // TODO
   const audioRef = useRef(null);
+  const [position, setPosition] = React.useState(32);
 
   function setCurrentTime(lineIndex) {
 
@@ -28,6 +33,40 @@ export default function AudioTextLines() {
   return (
     <>
       <Audio ref={audioRef} />
+      <Slider
+        aria-label="time-indicator"
+        size="small"
+        value={position}
+        min={0}
+        step={1}
+        max={100}
+        onChange={(_, value) => setPosition(value as number)}
+        sx={{
+          color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+          height: 4,
+          '& .MuiSlider-thumb': {
+            width: 8,
+            height: 8,
+            transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+            '&:before': {
+              boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+            },
+            '&:hover, &.Mui-focusVisible': {
+              boxShadow: `0px 0px 0px 8px ${theme.palette.mode === 'dark'
+                ? 'rgb(255 255 255 / 16%)'
+                : 'rgb(0 0 0 / 16%)'
+                }`,
+            },
+            '&.Mui-active': {
+              width: 20,
+              height: 20,
+            },
+          },
+          '& .MuiSlider-rail': {
+            opacity: 0.28,
+          },
+        }}
+      />
       <Container>
         <TextLines
           getCurrentIndex={getCurrentIndex}
