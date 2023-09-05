11f40cb6fdf8d2842ad4b6aeda9f4592271e4e93
vitaliiboiko360 <vukelan@gmail.com>
2023-09-05 22:12:08 +0300

    added slider and time

--- app-008-lang/src/audio_textlines.tsx
+++ app-008-lang/src/audio_textlines.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-import { useRef } from 'react';
-import { useTheme } from '@mui/material/styles';
+import { useRef, useCallback } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
 import Slider from '@mui/material/Slider';
 
 import TextLines from './text_lines.tsx';
 import Audio from './audio.tsx';
 
+const TinyText = styled(Typography)({
+  fontSize: '0.75rem',
+  opacity: 0.38,
+  fontWeight: 500,
+  letterSpacing: 0.2,
+});
+
 export default function AudioTextLines() {
   const theme = useTheme();
   const numChunks = 26; // TODO
   const audioRef = useRef(null);
-  const [position, setPosition] = React.useState(32);
+  const [position, setPosition] = React.useState(0);
+
+  // const setCurrentTime2 = useCallback((lineIndex) => {
+  //   const totalTime = audioRef.current.duration;
+  //   console.log(`duration ${totalTime}`);
+  //   audioRef.current.currentTime = (totalTime / numChunks) * lineIndex;
+  //   audioRef.current.play();
+
+  //   console.log(`set current time to ${(totalTime / numChunks) * lineIndex}`);
+  //   console.log(`clicked with index=${lineIndex}`);
+  // }, [setPosition]);
 
   function setCurrentTime(lineIndex) {
-
     const totalTime = audioRef.current.duration;
     console.log(`duration ${totalTime}`);
     audioRef.current.currentTime = (totalTime / numChunks) * lineIndex;
@@ -28,6 +45,12 @@ export default function AudioTextLines() {
   function getCurrentIndex() {
     const totalTime = audioRef.current.duration;
     return Math.floor((audioRef.current.currentTime * numChunks) / totalTime);
+  }
+
+  function formatDuration(value: number) {
+    const minute = Math.floor(value / 60);
+    const secondLeft = value - minute * 60;
+    return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
   }
 
   return (
@@ -67,6 +90,18 @@ export default function AudioTextLines() {
           },
         }}
       />
+      <TinyText sx={{
+        marginRight: 'auto',
+        marginLeft: 0,
+        display: 'inline',
+        float: 'left'
+      }}>{formatDuration(position)}</TinyText>
+      <TinyText sx={{
+        marginLeft: 'auto',
+        marginRight: 0,
+        display: 'inline',
+        float: 'right'
+      }}>{formatDuration(position)}</TinyText>
       <Container>
         <TextLines
           getCurrentIndex={getCurrentIndex}
