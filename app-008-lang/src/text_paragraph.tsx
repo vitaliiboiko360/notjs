a20d036d4154ac4cc8b34cb4e2efc6132f38c216
vitaliiboiko360 <vukelan@gmail.com>
2023-09-30 22:44:44 +0300

    working animation

--- app-008-lang/src/text_paragraph.tsx
+++ app-008-lang/src/text_paragraph.tsx
@@ -65,20 +65,21 @@ const StyledTypography = styled(Typography, {
 }));
 
 // If applying attributes, they need to be in the format {'attr':'val','attr2':'val2',...}
-function appendSVGChild(elementType: string, target: HTMLElement | SVGElement, attributes: Record<string, unknown> = {}, duration, to) {
+function appendSVGChild(elementType: string, target: HTMLElement | SVGElement, attributes: Record<string, unknown> = {}, duration, to, animationId, beginAnimation) {
   const schema = 'http://www.w3.org/2000/svg';
   const element: SVGElement = document.createElementNS(schema, elementType);
   Object.entries(attributes).map(a => element.setAttribute(a[0], a[1] as string));
-  let dur = '5s'
+  let dur = '3s'
   if (duration) {
     dur = duration
   }
 
   const animation = document.createElementNS(schema, "animate");
-  animation.setAttribute("attributeName", "width");
-  animation.setAttribute('from', '0');
+  animation.setAttribute('id', animationId);
+  animation.setAttribute('attributeName', 'width');
+  animation.setAttribute('from', 0);
   animation.setAttribute('to', to);
-  animation.setAttribute('begin', '0s');
+  animation.setAttribute('begin', 'indefinite');
   animation.setAttribute('dur', dur);
   animation.setAttribute('repeatCount', '1');
 
@@ -97,14 +98,14 @@ export default function TextParagraph(props) {
     const parentRect = spanRef.current.getBoundingClientRect();
 
     const children = [].slice.call(spanRef.current.childNodes);
-    console.log(`\n${JSON.stringify(parentRect)}`);
+    // console.log(`\n${JSON.stringify(parentRect)}`);
     const coordinates = children.reduce((collectedArray, child, index) => {
 
       const childRect = child.getBoundingClientRect();
-      const x = (childRect.x - parentRect.x) > 0 ? childRect.x : 0;
-      const coordinateValues = { 'x': (childRect.x - parentRect.x), 'y': childRect.height * (collectedArray.length + 1), 'deltaX': (childRect.x - parentRect.x) + childRect.width, 'lineBottomY': childRect.bottom };
 
-      console.log(`word=${child.innerText}\t${JSON.stringify(childRect)}`);
+      const coordinateValues = { 'x': (childRect.x - parentRect.x), 'y': childRect.height * (collectedArray.length + 1), 'deltaX': childRect.width, 'lineBottomY': childRect.bottom };
+
+      // console.log(`word=${child.innerText}\t${JSON.stringify(childRect)}`);
 
       const foundIndex = collectedArray.findIndex((element) => {
         return element.lineBottomY === coordinateValues.lineBottomY;
@@ -125,16 +126,44 @@ export default function TextParagraph(props) {
       width += values.deltaX;
       return width;
     }, 0);
-    //
-    const animationElements = coordinates.map(values => {
+
+    const animationElements = coordinates.map((values, index) => {
       let portionOfLength = lineLength / values.deltaX;
-      let durationOfAnimation = Math.floor(length / portionOfLength);
+      let durationOfAnimation = Math.ceil(length / portionOfLength);
+      //const d = `M${values.x},${values.y} L${values.x + values.deltaX},${values.y}`;
+
+      let startTime = '0s';
+      let animationId = `animation_${props.index}_${index}`;
+      if (index > 0) {
+        let prevAnimationId = `animation_${props.index}_${index - 1}`;
+        startTime = `${prevAnimationId}.end`;
+        // let prevPortionOfLength = lineLength / coordinates[index - 1].deltaX;
+        // let prevDurationOfAnimation = Math.floor(length / prevPortionOfLength);
+        // startTime = `${prevDurationOfAnimation}s`;
+      }
+
       return appendSVGChild('rect', svgRef.current, {
-        'x': values.x, 'y': values.y, 'stroke-width': '3', 'stroke': 'blue', 'fill': 'blue', 'height': 1, 'width': values.deltaX
-      }, `${durationOfAnimation}s`, values.deltaX);
+        'x': values.x, 'y': values.y, 'stroke-width': '1', 'stroke': '#33adff', 'width': '0', 'height': '1px', 'rx': '1px', 'ry': '1px'
+      }, `${durationOfAnimation}s`, values.deltaX, animationId, startTime);
     });
 
-    animationElements.forEach(a => a.beginElement());
+    const runAnimation = function (index) {
+      animationElements[index].addEventListener("endEvent", () => {
+        console.log(`runAnimation ${index}`);
+
+        if (animationElements.length > (index + 1)) {
+          animationElements[index].parentNode.setAttribute('width', animationElements[index].getAttribute('to'));
+          runAnimation(index + 1);
+        }
+        else {
+          animationElements.forEach((element) => { element.parentNode.remove() });
+        }
+      });
+      animationElements[index].beginElement();
+    }
+    runAnimation(0);
+    //animationElements.forEach(a => a.beginElement());
+    //animationElements[0].beginElement();
   }
 
   function onClick() {
@@ -144,10 +173,10 @@ export default function TextParagraph(props) {
 
   React.useEffect(() => {
     const { width, height, top, left } = spanRef.current.getBoundingClientRect();
-    svgRef.current.style.width = Math.ceil(width) + 'px';
-    svgRef.current.style.height = Math.ceil(height) + 'px';
-    svgRef.current.style.top = top + 'px';
-    svgRef.current.style.left = left + 'px';
+    svgRef.current.style.width = `${Math.ceil(width) + 10}px`;
+    svgRef.current.style.height = `${Math.ceil(height) + 1}px`;
+    svgRef.current.style.top = Math.ceil(top) + 'px';
+    svgRef.current.style.left = Math.ceil(left) + 'px';
     if (!props.active) {
       return;
     }
@@ -161,7 +190,7 @@ export default function TextParagraph(props) {
   return (<>
     <div key={props.index} style={{ display: 'inline' }}>
       <svg ref={svgRef} style={{ position: 'absolute', zIndex: '-1' }}></svg>
-      <span key={0} ref={spanRef} style={{ fontSize: 22, display: 'inline', }} onClick={onClick}>{wordsInSpans}</span>
+      <span ref={spanRef} style={{ fontSize: 22, display: 'inline', }} onClick={onClick}>{wordsInSpans}</span>
     </div >
   </>);
 }
