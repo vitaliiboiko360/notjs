8c0f58a54f24ce47072caae5dac69805d908ea22
vitaliiboiko360 <vukelan@gmail.com>
2023-12-15 20:30:59 +0200

    add grid to home

--- app-008-lang/src/home.tsx
+++ app-008-lang/src/home.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 const queryClient = new QueryClient()
 
-
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -17,19 +16,15 @@ function HomeWithData() {
     fetch('http://192.168.1.12:4001/data/list_of_texts.json').then(res =>
       res.json()
     )
-  )
-
-  if (isLoading) return 'Loading...'
-
+  );
+  if (isLoading) return 'Loading...';
   if (error) return 'Error getting list of texts: ' + error.message;
 
+  const articles = data.map((element) => { `<div>${element}</div>` });
+
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{' '}
-      <strong>✨ {data.stargazers_count}</strong>{' '}
-      <strong>🍴 {data.forks_count}</strong>
+    <div class="home.grid">
+      {articles}
     </div>
   );
 }
