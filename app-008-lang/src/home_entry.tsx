317bf45127d69c7fcee2a8ec24f24275b22adb0e
vitaliiboiko360 <vukelan@gmail.com>
2024-01-04 15:58:25 +0200

    starting styling

--- app-008-lang/src/home_entry.tsx
+++ app-008-lang/src/home_entry.tsx
@@ -5,7 +5,7 @@ import { makeUrlToArtwork } from './util.ts'
 export default function HomeEntry(props) {
   return (<>
     <div>
-      <div>
+      <div className='artwork'>
         <img src={makeUrlToArtwork(props.artwork)} />
       </div>
       <a href={props.href} >{props.title}</a>
