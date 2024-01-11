c1783c2945005d4d710d73ecc262ce1b9c8b8997
vitaliiboiko360 <vukelan@gmail.com>
2024-01-11 18:44:27 +0200

    writing our own control

--- app-008-lang/src/various_controls/playback_rate.tsx
+++ app-008-lang/src/various_controls/playback_rate.tsx
@@ -1,39 +1,41 @@
 import React from 'react'
 import { forwardRef } from 'react';
 
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
 import SpeedIcon from '@mui/icons-material/Speed';
 
 const PlaybackRateDropdown = forwardRef((props, ref) => {
   const [playbackSpeed, setPlaybackSpeed] = React.useState(1.0);
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setPlaybackSpeed(event.target.value);
-    ref.current.playbackRate = event.target.value;
+  const handleChange = (value) => {
+    setPlaybackSpeed(value);
+    ref.current.playbackRate = value;
+  };
+
+  const liItems = React.useRef([null, null, null]);
+
+  const onClick = (value) => {
+    console.log(value)
   };
 
   const values = [1.0, 0.85, 0.7];
+  const style = {
+    'listStyle': 'none',
+    'fontSize': '1.5em',
+  }
   return (
     <>
       <div>
         <SpeedIcon>Playback Speed</SpeedIcon>
       </div>
       <div>
-        <FormControl variant="filled" sx={{ m: 1, minWidth: 60 }}>
-          <Select
-            value={playbackSpeed}
-            onChange={handleChange}
-          >
-            {values.map((value) => {
-              if (value != playbackSpeed)
-                return <MenuItem value={value}>{value}</MenuItem>
-              return <MenuItem sx={{ display: 'none' }} value={value}>{value}</MenuItem>
-            })}
-          </Select>
-        </FormControl>
+        <ul style={style}>
+          {/* <li value={1.0}>1.0</li>
+          <li value={0.85}>0.85</li>
+          <li value={0.7}>0.7</li> */}
+          {values.map((value, index) => {
+            return (<li ref={liItems[index]} style={(value != playbackSpeed) ? { 'display': 'none' } : {}} onClick={() => onClick(value)} value={value} key={index}>{value}</li>)
+          })}
+        </ul>
       </div >
     </>
   )
