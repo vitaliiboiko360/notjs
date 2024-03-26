f658b7366388814154e5b6ec8cd615d7176fbf87
vitaliiboiko360 <vukelan@gmail.com>
2024-03-26 12:00:26 +0200

    rem setviewport

--- pup2/src/main.ts
+++ pup2/src/main.ts
@@ -2,12 +2,13 @@ import puppeteer from 'puppeteer';
 import { logger } from './Logger';
 
 async function main() {
-  logger.info('Launching headless Chrome to perform the OpenID Connect dance...');
+  logger.info('Launching headless Chrome');
 
   const browser = await puppeteer.launch({
     executablePath: '/opt/google/chrome/google-chrome',
     headless: false,
     userDataDir: '/home/user1/.config/google-chrome/Default',
+    defaultViewport: null,
     args: [
       '--user-data-dir="/home/user1/.config/google-chrome/Default"',
       '--disable-dev-shm-usage',
@@ -20,6 +21,7 @@ async function main() {
 
   logger.info('Navigating to login page...');
   await page.goto('https://translate.google.com/', { waitUntil: 'load' });
+
 }
 
 main();
