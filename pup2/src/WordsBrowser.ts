f35c0217fa77a3ea19dbfc14ef023ac6bd4e7b10
vitaliiboiko360 <vukelan@gmail.com>
2024-03-27 23:08:31 +0200

    paste text

--- pup2/src/WordsBrowser.ts
+++ pup2/src/WordsBrowser.ts
@@ -1,6 +1,10 @@
 import puppeteer from 'puppeteer';
 
 export async function getWordsJson(page: puppeteer.Page, strInput: string): Promise<Object> {
-
+  const inputTextFieldBox = '#rm1UF UnxENd';
+  await page.waitForSelector(inputTextFieldBox, {
+    visible: true
+  });
+  await page.type(inputTextFieldBox, "¿Te gusta leer?");
   return {};
 }
