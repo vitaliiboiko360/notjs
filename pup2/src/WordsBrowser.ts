b206441d3930a8bbc73f6add81ed92d3c75a00d7
vitaliiboiko360 <vukelan@gmail.com>
2024-03-30 22:22:18 +0200

    rework evaluate

--- pup2/src/WordsBrowser.ts
+++ pup2/src/WordsBrowser.ts
@@ -1,6 +1,10 @@
 import puppeteer from 'puppeteer';
 import { logger } from './Logger';
 
+function wait(ms: number) {
+  return new Promise((r) => setTimeout(r, ms));
+}
+
 function run(func: Function, message: string) {
   try {
     func();
@@ -11,15 +15,24 @@ function run(func: Function, message: string) {
 
 export async function getWordsJson(page: puppeteer.Page, strInput: string): Promise<Object> {
   const inputTextFieldBox = '.QFw9Te textarea';
+  const localInput = "¿Te gusta leer?";
 
-  let elementHandle = await page.waitForSelector(inputTextFieldBox, { visible: true });
+  await page.waitForSelector(inputTextFieldBox, { visible: true });
 
-  run(async () => await page.type(inputTextFieldBox, "¿Te gusta leer?"), 'page.type');
-
-  //await page.waitForSelector(inputTextFieldBox, { visible: true });
-  await elementHandle.focus();
-
-  await page.evaluate(textarea => textarea.setSelectionRange(0, 3), elementHandle);
+  run(async () => await page.type(inputTextFieldBox, localInput), 'page.type');
+  logger.info('after type');
+  await page.evaluate((selector, localInput) => {
+    let ta = document.querySelectorAll(selector);
+    if (ta.length == 0) {
+      return;
+    }
+    let textArea = ta[0];
+    localInput.split(' ').reduce(async (start: number, word: string) => {
+      let end = start + word.length;
+      textArea.setSelectionRange(start, end);
+      await wait(3000);
+    });
+  }, inputTextFieldBox, localInput);
   logger.info('after evaluate');
   return {};
 }
