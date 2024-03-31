a993795a8b029058a4e4988bdba65c19b28eeaa6
vitaliiboiko360 <vukelan@gmail.com>
2024-03-31 21:44:47 +0300

    reduce return number

--- pup2/src/WordsBrowser.ts
+++ pup2/src/WordsBrowser.ts
@@ -38,25 +38,26 @@ export async function getWordsJson(page: puppeteer.Page, strInput: string): Prom
     localInput
       .split(' ')
       .reduce(
-        async (prevPromise: Promise<void>, word: string, index: number, array: [string]) => {
-          await prevPromise;
+        async (prevPromise: Promise<number>, word: string, index: number, array: [string]) => {
+          let start = await prevPromise;
           return wait(3000).then(
             () => {
               console.log(`new promise index=${index}`);
-              let start = (index > 0)
-                ? array.slice(0, index)
-                  .reduce(
-                    (t: number, v: string): number => {
-                      return t + v.length + 1; /*+1 for space*/
-                    }, 0
-                  )
-                : 0;
+              // let start = (index > 0)
+              //   ? array.slice(0, index)
+              //     .reduce(
+              //       (t: number, v: string): number => {
+              //         return t + v.length + 1; /*+1 for space*/
+              //       }, 0
+              //     )
+              //   : 0;
               let end = start + word.length;
               textArea.focus();
               textArea.setSelectionRange(start, end);
               console.log(`setSelectionRange(${start}, ${end})`);
+              return end + 1;
             });
-        }, Promise.resolve());
+        }, Promise.resolve(0));
 
   }, inputTextFieldBox, localInput);
 
