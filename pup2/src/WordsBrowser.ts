fd525a0e7c8a1ed475af8d663dcca2bf2f0d17e6
vitaliiboiko360 <vukelan@gmail.com>
2024-04-01 21:51:59 +0300

    re-entering textarea doesnt work

--- pup2/src/WordsBrowser.ts
+++ pup2/src/WordsBrowser.ts
@@ -23,34 +23,52 @@ export async function getWordsJson(page: puppeteer.Page, strInput: string): Prom
   logger.info('after type');
 
   await page.evaluate((selector, localInput) => {
+    const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
     let ta = document.querySelectorAll(selector);
     console.log(`ta length ${ta.length}`);
     if (ta.length == 0) {
       return;
     }
-    console.log(`localInput ${localInput}\n${(ta[0])}`);
+    console.log(`1 localInput ${localInput}\n${(ta[0])}`);
     let textArea = ta[0];
-    textArea.focus();
-
-
-    const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    localInput
-      .split(' ')
-      .reduce(
-        async (prevPromise: Promise<number>, word: string, index: number, array: [string]) => {
-          let start = await prevPromise;
-          return wait(3000).then(
-            () => {
-              let end = start + word.length;
-              textArea.focus();
-              textArea.setSelectionRange(start, end);
-              return end + 1;
-            });
-        }, Promise.resolve(0));
-
+    wait(1000)
+      .then(() => {
+        textArea.setSelectionRange(0, localInput.split(' ')[0].length);
+      });
   }, inputTextFieldBox, localInput);
+  logger.info('after 1 evaluate');
 
-  logger.info('after evaluate');
+  let seeDictButton = ".VfPpkd-StrnGf-rymPhb.DMZ54e.vQXW9e";
+  let button = await page.waitForSelector(seeDictButton, { visible: true });
+  if (button) {
+    console.log('click');
+    await button.click();
+  }
+
+
+  // await page.evaluate((selector, localInput) => {
+  //   // const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  //   // let ta = document.querySelectorAll(selector);
+  //   // console.log(`ta length ${ta.length}`);
+  //   // if (ta.length == 0) {
+  //   //   return;
+  //   // }
+  //   // console.log(`2 localInput ${localInput}\n${(ta[0])}`);
+  //   // let textArea = ta[0];
+
+  //   // localInput
+  //   //   .split(' ')
+  //   //   .reduce(
+  //   //     async (prevPromise: Promise<number>, word: string, index: number, array: [string]) => {
+  //   //       let start = await prevPromise;
+  //   //       return wait(3000).then(
+  //   //         () => {
+  //   //           let end = start + word.length;
+  //   //           //textArea.setSelectionRange(start, end);
+  //   //           return end + 1;
+  //   //         });
+  //   //     }, Promise.resolve(0));
+  // }, inputTextFieldBox, localInput);
+
   return {};
 }
