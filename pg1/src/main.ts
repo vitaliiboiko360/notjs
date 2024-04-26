8e6168dfb2f951d9dfb428840aeb6b3b042457ea
vitaliiboiko360 <vukelan@gmail.com>
2024-04-26 22:02:21 +0300

    connection setup

---
+++ pg1/src/main.ts
@@ -0,0 +1,24 @@
+
+import pg from 'pg';
+
+const pgTypes = require('pg').types
+const { Client } = pg;
+
+const clientConfig = {
+  host: "192.168.1.3",
+  port: 5334,
+  user: "learntospeak",
+  database: "learntospeak",
+  password: "5b553db13b5157677f76394ace066fbe",
+  application_name: "db client",
+  keepAlive: true,
+};
+
+
+const client = new Client(clientConfig)
+await client.connect()
+
+const res = await client.query('SELECT NOW()');
+console.log(res);
+
+await client.end();
