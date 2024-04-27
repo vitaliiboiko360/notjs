f9061d10e1e0e8b0142e4b297ed426fc0a77565f
vitaliiboiko360 <vukelan@gmail.com>
2024-04-27 13:11:38 +0300

    adjust connection settings

--- pg1/src/main.ts
+++ pg1/src/main.ts
@@ -5,10 +5,11 @@ const { Client } = pg;
 
 const clientConfig = {
   host: "192.168.1.3",
-  port: 5334,
+  port: 5432,
   user: "learntospeak",
   database: "learntospeak",
   password: "5b553db13b5157677f76394ace066fbe",
+  ssl: false,
   application_name: "db client",
   keepAlive: true,
 };
