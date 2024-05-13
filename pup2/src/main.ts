5158caddbef8d6013799fa0398864f8088fa1fb5
vitaliiboiko360 <vukelan@gmail.com>
2024-05-13 14:01:02 +0300

    add config file

--- pup2/src/main.ts
+++ pup2/src/main.ts
@@ -4,10 +4,14 @@ import { logger } from './Logger';
 import { getWordsJson } from './WordsBrowser';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import fs from 'node:fs';
 
 const rl = readline.createInterface({ input, output });
 
 async function main() {
+
+  let config = fs.readFileSync(__dirname + '/../config.conf', 'utf8');
+
   logger.info('Launching headless Chrome');
 
   const browser = await puppeteer.launch({
@@ -25,9 +29,6 @@ async function main() {
     ]
   });
 
-  let index = 0;
-  const localInput = ["¿Te gusta leer?", "Yo leo un libro todas las semanas.", "Y a veces, leo dos libros."];
-
   const page = (await browser.pages())[0];
   await page.bringToFront();
 
@@ -35,16 +36,19 @@ async function main() {
 
   await page.goto('https://translate.google.com/', { waitUntil: 'load' });
 
+
+  let index = 0;
+  const localInput = ["¿Te gusta leer?", "Yo leo un libro todas las semanas.", "Y a veces, leo dos libros."];
+
   async function getResultAndOutput(page: puppeteer.Page, inputString: string) {
     let result = await getWordsJson(page, inputString);
     console.log(`\n${JSON.stringify(result, null, 2)}\n`);
   }
 
-  await getResultAndOutput(page, localInput[index])
   while (true) {
     const answer: string = await rl.question('Run next iteration? type y if yes ');
     if (answer == 'y') {
-      await getResultAndOutput(page, localInput[++index % localInput.length]);
+      await getResultAndOutput(page, localInput[index++ % localInput.length]);
     }
     if (answer == 'q') {
       process.exit();
