aa5e991d204f7144f384871918a53b4f9367729c
vitaliiboiko360 <vukelan@gmail.com>
2024-05-27 23:35:05 +0300

    add connection handler

--- wuna/src/WebSocketServer.ts
+++ wuna/src/WebSocketServer.ts
@@ -22,3 +22,15 @@ const wss = new WebSocketServer({
     // should not be compressed if context takeover is disabled.
   }
 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
