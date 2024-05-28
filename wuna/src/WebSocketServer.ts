cf4fbe439000d18c4cd6de425e809cf76c184ff3
vitaliiboiko360 <vukelan@gmail.com>
2024-05-28 23:09:46 +0300

    formating

--- wuna/src/WebSocketServer.ts
+++ wuna/src/WebSocketServer.ts
@@ -23,14 +23,15 @@ const wss = new WebSocketServer({
   }
 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on('connection',
+  function connection(ws) {
+    ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
+    ws.on('message', function message(data, isBinary) {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
     });
   });
-});
