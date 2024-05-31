10683e9d7dd7db927c19aadd274cda45f7c8c00b
vitaliiboiko360 <vukelan@gmail.com>
2024-05-31 16:56:54 +0300

    correct path

--- wuna/startWebSockertServer.sh
+++ wuna/startWebSockertServer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 tsc
 for file in ./out/*.js; do
-    mv "$file" "$(basename "$file" .js).mjs"
+    mv "$file" "./out/$(basename "$file" .js).mjs"
 done
 node ./out/WebSocketServer.mjs
