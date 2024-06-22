b95b88544c33c240b5e66adab7dcda50cc76487b
vitaliiboiko360 <vukelan@gmail.com>
2024-06-22 23:11:12 +0300

    rework center card stack

--- wuna/cli/svg/svg_cardsstack.tsx
+++ wuna/cli/svg/svg_cardsstack.tsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Card from './svg_card';
 import { SVG_DIMENSIONS } from './svg_container';
+import { WebSocketContext } from '../websocketprovider'
 
 const xCenter = SVG_DIMENSIONS.width / 2;
 const yCenter = SVG_DIMENSIONS.height / 2;
@@ -8,9 +9,23 @@ const yCenter = SVG_DIMENSIONS.height / 2;
 const xPos = xCenter / 2;
 const yPos = yCenter / 2;
 
-const k = 2;
-
 export default function SvgCardStack(props) {
+  const webSocket = useContext(WebSocketContext);
+  useEffect(() => {
+    const onMessage = (event) => {
+      let arBuf = new Uint8Array(event.data);
+      console.log(`from onMessage inside card stack`);
+      setWhichCard(arBuf[0]);
+    };
+
+    webSocket
+      .addEventListener("message", onMessage);
+
+    return () => {
+      webSocket
+        .removeEventListener("message", onMessage);
+    };
+  }, []);
   const svgCardStack_ref = useRef(null);
   return (<svg ref={svgCardStack_ref} x={xPos} y={yPos} width={xCenter} height={yCenter} >
     <Card ref={svgCardStack_ref} />
