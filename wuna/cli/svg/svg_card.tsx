cd0024e99b82fcd2c4f2a72919caa238bc3a8e54
vitaliiboiko360 <vukelan@gmail.com>
2024-06-23 11:17:55 +0300

    need append element

--- wuna/cli/svg/svg_card.tsx
+++ wuna/cli/svg/svg_card.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useContext, useEffect, forwardRef } from 'react';
 import { WebSocketContext } from '../websocketprovider'
 
-const Card = forwardRef((props) => {
-  const [whichCard, setWhichCard] = useState(0);
+const Card = forwardRef((props, svgCardStack_ref) => {
+  const [whichCard, setWhichCard] = useState(-1);
   const webSocket = useContext(WebSocketContext);
   useEffect(() => {
     const onMessage = (event) => {
@@ -12,11 +12,19 @@ const Card = forwardRef((props) => {
 
     if (svgCardStack_ref.current == null) {
       console.log(`svg element isn't ready yet`);
+      return;
     }
 
+    webSocket.addEventListener("message", onMessage);
 
-    return (<></>);
+    return () => webSocket.removeEventLisnter("message", onMessage);
   });
+
+  if (whichCard != -1) {
+
+  }
+
+  return (<></>);
 });
 
 export default Card;
