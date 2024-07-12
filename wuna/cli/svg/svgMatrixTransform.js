f07af43d7552431569ab3cf000890b790b0b4a61
vitaliiboiko360 <vukelan@gmail.com>
2024-07-12 13:44:02 +0300

    add script update test

--- wuna/cli/svg/svgMatrixTransform.js
+++ wuna/cli/svg/svgMatrixTransform.js
@@ -4,8 +4,18 @@ let text1 = document.getElementById('textDisplay0');
 
 let interval1 = setInterval(() => {
   angle1 = (angle1 + 0.1) % 2;
+  text1.textContent = `${angle1}`;
   const transformString = `matrix(${Math.cos(Math.PI * angle1)},${Math.sin(Math.PI * angle1)},${-Math.sin(Math.PI * angle1)},${Math.cos(Math.PI * angle1)},0,0)`;
   control1.setAttribute('transform', transformString);
 }, 1200);
 
 setInterval(_ => clearInterval(interval1), 100000);
+
+clearInterval(interval1);
+angle1 = 0;
+interval1 = setInterval(() => {
+  angle1 = (angle1 + 0.1) % 2;
+  text1.textContent = `${angle1 * 180 < 0.0001 ? 0 : angle1 * 180}`;
+  const transformString = `matrix(${Math.cos(Math.PI / 2)},${Math.sin(Math.PI * angle1)},${-Math.sin(Math.PI * angle1)},${Math.cos(Math.PI / 2)},0,0)`;
+  control1.setAttribute('transform', transformString);
+}, 1200);
