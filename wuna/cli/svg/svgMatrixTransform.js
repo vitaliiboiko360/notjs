93f80e6b13d39b42a4dcdc8627780d40e826e193
vitaliiboiko360 <vukelan@gmail.com>
2024-07-13 11:09:03 +0300

    update test

--- wuna/cli/svg/svgMatrixTransform.js
+++ wuna/cli/svg/svgMatrixTransform.js
@@ -1,6 +1,7 @@
 let angle1 = 0;
 let control1 = document.getElementById('controlTransform0');
 let text1 = document.getElementById('textDisplay0');
+let text1_1 = document.getElementById('textDisplay_0_1');
 
 let interval1 = setInterval(() => {
   angle1 = (angle1 + 0.1) % 2;
@@ -16,6 +17,12 @@ angle1 = 0;
 interval1 = setInterval(() => {
   angle1 = (angle1 + 0.1) % 2;
   text1.textContent = `${angle1 * 180 < 0.0001 ? 0 : angle1 * 180}`;
-  const transformString = `matrix(${Math.cos(Math.PI / 2)},${Math.sin(Math.PI * angle1)},${-Math.sin(Math.PI * angle1)},${Math.cos(Math.PI / 2)},0,0)`;
+  const transformString = `matrix(${Math.cos(Math.PI * angle1)},${Math.sin(Math.PI * angle1)},${-Math.sin(Math.PI * angle1)},${Math.cos(Math.PI * angle1)},0,0)`;
   control1.setAttribute('transform', transformString);
+
+  let { x, y, width, height } = control1.getBoundingClientRect();
+  let cx = width / 2 + x;
+  let cy = height / 2 + y;
+  const stringToOutput = `x: ${cx}  y:${cy}`;
+  text1_1.textContent = stringToOutput;
 }, 1200);
