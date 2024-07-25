6842ab71415673910f736e4b181f912abc96b2b2
vitaliiboiko360 <vukelan@gmail.com>
2024-07-25 21:39:12 +0300

    make active card hand denser

--- wuna/cli/svg/svg_activeplayercards.tsx
+++ wuna/cli/svg/svg_activeplayercards.tsx
@@ -11,8 +11,8 @@ export default function SvgActivePlayerCards(props) {
   //console.log(`props.cardsArray.length=${props.cardArray.length}`);
 
   return (<>
-    {props.cardArray.slice(0, 10).map((card, index) => {
-      const transformString = `translate(${index * 20})`; // 20px -> step to the right on X axe
+    {props.cardArray.slice(0, 15).map((card, index) => {
+      const transformString = `translate(${index * 15})`; // 20px -> step to the right on X axe
       return (<Fragment key={index}>
         <g transform={transformString} >
           {getCard(card)}
