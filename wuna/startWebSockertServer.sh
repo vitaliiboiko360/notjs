cbb812b96de6a53cedb947704b887a9113bf8f18
vitaliiboiko360 <vukelan@gmail.com>
2024-07-27 13:33:18 +0300

    testing with script

--- wuna/startWebSockertServer.sh
+++ wuna/startWebSockertServer.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
-tsc
-for file in ./out/*.js; do
+#tsc
+for file in ./out/*.mjs; do
+    # 
+    # example of expression
+    # sed -i -E "s/(import.*PlayerWsConnection)/\1\.mjs/" WebSocketServer.mjs
+    #fileName = eval $(basename $file .mjs)
+    #echo "${fileName}"
+    #if [ "${fileName}" = "WebSocketServer" ]; then
+      # echo "$file"
+      # echo "echo s\/(import.*PlayerWsConnection)\/\1\.mjs\/" "$file"
+      echo ''
+    #fi
+
     mv "$file" "./out/$(basename "$file" .js).mjs"
 done
-node ./out/WebSocketServer.mjs
+# node ./out/WebSocketServer.mjs
