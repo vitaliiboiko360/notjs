c5de830a7a0cd3e43afc0dd282e1cc5fe056ef1e
vitaliiboiko360 <vukelan@gmail.com>
2024-07-31 23:17:42 +0300

    move onmessage to read msg

--- wuna/cli/websocketconsumer.tsx
+++ wuna/cli/websocketconsumer.tsx
@@ -39,37 +39,41 @@ function isValidCard(idOfCard: number) {
 
 let staticCounter = 0;
 
+function readMessage(inputArray: Uint8Array, dispatch) {
+  for (let i = 0; i < inputArray.length; ++i) {
+    let number = inputArray[i];
+    if (isValidCard(number)) {
+      dispatch(updateActiveCards(number));
+
+      if (getColor(number) == COLOR.BLUE) {
+        dispatch(incrementLeftUserCardsNumber());
+      }
+
+      if (getColor(number) == COLOR.RED) {
+        dispatch(incrementTopUserCardsNumber());
+      }
+
+      if (getColor(number) == COLOR.GREEN) {
+        dispatch(incrementRightUserCardsNumber());
+      }
+
+      staticCounter++;
+      if (staticCounter == 3) {
+        // console.log('Dispatch active table top card =', number);
+        dispatch(updateActiveTableTopCard(number));
+        staticCounter = 0;
+      }
+    }
+  }
+}
+
 export default function WebSocketConsumer(props) {
   const webSocket = useContext(WebSocketContext);
   const dispatch = useAppDispatch();
 
   const onMessage = useCallback((event) => {
     let arBuf = new Uint8Array(event.data);
-    for (let i = 0; i < arBuf.length; ++i) {
-      let number = arBuf[i];
-      if (isValidCard(number)) {
-        dispatch(updateActiveCards(number));
-
-        if (getColor(number) == COLOR.BLUE) {
-          dispatch(incrementLeftUserCardsNumber());
-        }
-
-        if (getColor(number) == COLOR.RED) {
-          dispatch(incrementTopUserCardsNumber());
-        }
-
-        if (getColor(number) == COLOR.GREEN) {
-          dispatch(incrementRightUserCardsNumber());
-        }
-
-        staticCounter++;
-        if (staticCounter == 3) {
-          // console.log('Dispatch active table top card =', number);
-          dispatch(updateActiveTableTopCard(number));
-          staticCounter = 0;
-        }
-      }
-    }
+    readMessage(arBuf, dispatch);
   }, [dispatch]);
 
   useEffect(() => {
