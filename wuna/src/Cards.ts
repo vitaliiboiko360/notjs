0f5b0032052c48c0933ac4701ab6553d83048157
vitaliiboiko360 <vukelan@gmail.com>
2024-08-07 00:03:25 +0300

    rename enum entries

--- wuna/src/Cards.ts
+++ wuna/src/Cards.ts
@@ -12,84 +12,84 @@ export enum UNIVERSAL {
 
 // red, green, blue, yelow
 export enum RED {
-  RED_0 = 20,
-  RED_1,
-  RED_2,
-  RED_3,
-  RED_4,
-  RED_5,
-  RED_6,
-  RED_7,
-  RED_8,
-  RED_9,
-  RED_Reverse,
-  RED_SkipStep,
-  RED_PlusTwo
+  _0 = 20,
+  _1,
+  _2,
+  _3,
+  _4,
+  _5,
+  _6,
+  _7,
+  _8,
+  _9,
+  _Reverse,
+  _SkipStep,
+  _PlusTwo
 }
 
 export enum GREEN {
-  GREEN_0 = 40,
-  GREEN_1,
-  GREEN_2,
-  GREEN_3,
-  GREEN_4,
-  GREEN_5,
-  GREEN_6,
-  GREEN_7,
-  GREEN_8,
-  GREEN_9,
-  GREEN_Reverse,
-  GREEN_SkipStep,
-  GREEN_PlusTwo
+  _0 = 40,
+  _1,
+  _2,
+  _3,
+  _4,
+  _5,
+  _6,
+  _7,
+  _8,
+  _9,
+  _Reverse,
+  _SkipStep,
+  _PlusTwo
 }
 
 export enum BLUE {
-  BLUE_0 = 60,
-  BLUE_1,
-  BLUE_2,
-  BLUE_3,
-  BLUE_4,
-  BLUE_5,
-  BLUE_6,
-  BLUE_7,
-  BLUE_8,
-  BLUE_9,
-  BLUE_Reverse,
-  BLUE_SkipStep,
-  BLUE_PlusTwo
+  _0 = 60,
+  _1,
+  _2,
+  _3,
+  _4,
+  _5,
+  _6,
+  _7,
+  _8,
+  _9,
+  _Reverse,
+  _SkipStep,
+  _PlusTwo
 }
 
 export enum YELLOW {
-  YELLOW_0 = 80,
-  YELLOW_1,
-  YELLOW_2,
-  YELLOW_3,
-  YELLOW_4,
-  YELLOW_5,
-  YELLOW_6,
-  YELLOW_7,
-  YELLOW_8,
-  YELLOW_9,
-  YELLOW_Reverse,
-  YELLOW_SkipStep,
-  YELLOW_PlusTwo
+  _0 = 80,
+  _1,
+  _2,
+  _3,
+  _4,
+  _5,
+  _6,
+  _7,
+  _8,
+  _9,
+  _Reverse,
+  _SkipStep,
+  _PlusTwo
 }
 
 export function isValidCard(idOfCard: number) {
   if (idOfCard == UNIVERSAL.UNIVERSAL_PlusFour
     || idOfCard == UNIVERSAL.UNIVERSAL)
     return true;
-  if (idOfCard >= RED.RED_0
-    && idOfCard <= RED.RED_PlusTwo)
+  if (idOfCard >= RED._0
+    && idOfCard <= RED._PlusTwo)
     return true;
-  if (idOfCard >= GREEN.GREEN_0
-    && idOfCard <= GREEN.GREEN_PlusTwo)
+  if (idOfCard >= GREEN._0
+    && idOfCard <= GREEN._PlusTwo)
     return true;
-  if (idOfCard >= BLUE.BLUE_0
-    && idOfCard <= BLUE.BLUE_PlusTwo)
+  if (idOfCard >= BLUE._0
+    && idOfCard <= BLUE._PlusTwo)
     return true;
-  if (idOfCard >= YELLOW.YELLOW_0
-    && idOfCard <= YELLOW.YELLOW_PlusTwo)
+  if (idOfCard >= YELLOW._0
+    && idOfCard <= YELLOW._PlusTwo)
     return true;
 
   return false;
