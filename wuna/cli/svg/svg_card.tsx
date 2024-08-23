29a457785fe75b8c08707c61e519ab9b086c20e1
vitaliiboiko360 <vukelan@gmail.com>
2024-08-23 22:19:53 +0300

    rework card stack remove unneccessary props; add lastplayer info to rendering component

--- wuna/cli/svg/svg_card.tsx
+++ wuna/cli/svg/svg_card.tsx
@@ -1,16 +1,26 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
 import { getCard } from './svg_getcard';
 
-import { selectActiveMoveCard } from '../store/activeMove.ts';
+import { selectActiveMoveCard, selectActiveMoveLastPlayer } from '../store/activeMove.ts';
 import { useAppSelector } from '../store/hooks.ts';
 
 import { renderToString } from 'react-dom/server';
+
+import { xCenter, yCenter } from './svg_cardsstack.tsx';
 
 const CARD_HALF_WIDTH = 32;
 const CARD_HALF_HEIGHT = 48;
 
 const Card = forwardRef((props, refGroupCenterTable) => {
+  const refPreviousMove = useRef({ topCard: 0, lastPlayer: 0 });
   const topCardId = useAppSelector(selectActiveMoveCard);
+  const lastPlayerId = useAppSelector(selectActiveMoveLastPlayer);
+
+  // if (topCardId != refPreviousMove.current.topCard)
+  //   refPreviousMove.current.topCard = topCardId;
+  // if (lastPlayerId != refPreviousMove.current.lastPlayer)
+  //   refPreviousMove.current.lastPlayer = lastPlayerId;
+
 
   if (topCardId == 0) {
     console.log('topCardId= ', topCardId);
@@ -21,18 +31,18 @@ const Card = forwardRef((props, refGroupCenterTable) => {
     console.log('GroupCenter Card no REF !!!! topCardId=', topCardId);
     return;
   }
-  const transformString = props.transformString;
-  // console.log('transformString=', transformString);
-  const [_0, _1, _2, _3, _4, xDelta, yDelta] = transformString.match(/matrix\(([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+)\)/);
-  // console.log('xDelta=', xDelta);
-  // console.log('yDelta=', yDelta);
+
   let x = Math.floor(Math.random() * CARD_HALF_WIDTH) + CARD_HALF_WIDTH;
   let y = Math.floor(Math.random() * CARD_HALF_HEIGHT) + CARD_HALF_HEIGHT;
-  // console.log(JSON.stringify(transformString.match(/matrix\(([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+),([0-9]+)\)/)));
+
   let element = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-  element.setAttribute('transform', `matrix(1,0,0,1,${xDelta - x},${yDelta - y})`);
+  element.setAttribute('transform', `matrix(1,0,0,1,${xCenter - x},${yCenter - y})`);
   element.innerHTML = renderToString(getCard(topCardId));
+
   refGroupCenterTable.current.append(element);
+
+  refPreviousMove.current.topCard = topCardId;
+  refPreviousMove.current.lastPlayer = lastPlayerId;
 
   return (<></>);
 });
--- wuna/cli/svg/svg_cardsstack.tsx
+++ wuna/cli/svg/svg_cardsstack.tsx
@@ -2,11 +2,11 @@ import React, { useRef, forwardRef } from 'react';
 import Card from './svg_card';
 import { SVG_DIMENSIONS } from './svg_container';
 
-const xCenter = SVG_DIMENSIONS.width / 2;
-const yCenter = SVG_DIMENSIONS.height / 2;
+export const xCenter = SVG_DIMENSIONS.width / 2;
+export const yCenter = SVG_DIMENSIONS.height / 2;
 const transformString = `matrix(1,0,0,1,${xCenter},${yCenter})`;
 
-const SvgForCards = forwardRef((props, refGroupCenterTable) => {
+const GroupForCards = forwardRef((props, refGroupCenterTable) => {
   return (<g ref={refGroupCenterTable} transfrom={transformString}  >
   </g>);
 });
@@ -14,7 +14,7 @@ const SvgForCards = forwardRef((props, refGroupCenterTable) => {
 export default function SvgCardStack(props) {
   const refGroupCenterTable = useRef(null);
   return (<>
-    <SvgForCards ref={refGroupCenterTable} />
-    <Card transformString={transformString} ref={refGroupCenterTable} />
+    <GroupForCards ref={refGroupCenterTable} />
+    <Card ref={refGroupCenterTable} />
   </>);
 }
