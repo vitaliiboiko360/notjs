74075ddf9b5d0be1593b286b30b608e9904e47a7
vitaliiboiko360 <vukelan@gmail.com>
2025-01-24 01:14:51 +0200

    correct typescript errors

--- pup2/src/GetTranslation.ts
+++ pup2/src/GetTranslation.ts
@@ -1,29 +1,28 @@
 import puppeteer from 'puppeteer';
 import { getWordsJson } from './WordsBrowser.js';
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-
-const rl = readline.createInterface({ input, output });
+import { readJsonFromFileSync, saveJsonToFileSync } from './fs/Json.js';
 
 declare type translation = { originalLine: string; translation: {} };
 
 export default async function getAndLoadTranslations(
   page: puppeteer.Page,
   inputFileName: string
-) {
-  let inputJson: any = await fetchJsonFileFromDb(inputFileName);
-  let textLines = inputJson.lines.map(
+): Promise<void> {
+  const inputJson: any = await readJsonFromFileSync(inputFileName);
+  const textLines = inputJson.lines.map(
     (element: { text: string }) => element.text
   );
 
-  let resultsJson: { translations: translation[] } = { translations: [] };
+  const resultsJson: { translations: Array<translation> } = {
+    translations: [],
+  };
   for (let i = 0; i < textLines.length; ++i) {
-    let inputString = textLines[i];
-    let result = await getWordsJson(page, inputString);
+    const inputString = textLines[i];
+    const result = await getWordsJson(page, inputString);
     resultsJson.translations.push({
       originalLine: inputString,
       translation: result,
     });
   }
-  saveJsonTranslationToDb(resultsJson, inputFileName);
+  saveJsonToFileSync(inputFileName, resultsJson);
 }
