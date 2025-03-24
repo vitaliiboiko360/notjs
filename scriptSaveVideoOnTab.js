ec6d374d641c919677e9c0c9b5847cec86993f4a
vitaliiboiko360 <vukelan@gmail.com>
2025-03-24 23:15:01 +0200

    start script

---
+++ scriptSaveVideoOnTab.js
@@ -0,0 +1,10 @@
+let divElement = document.createElement('div');
+document.body.append(divElement);
+divElement.style.position = 'absolute';
+divElement.style.top = 0;
+divElement.style.left = 0;
+divElement.style.height = '100px';
+divElement.style.width = '200px';
+divElement.style.backgroundColor = 'grey';
+
+let buttonSaveVideo = document.createElement('button');
